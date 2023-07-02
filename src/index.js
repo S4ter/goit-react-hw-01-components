5130ab5e524b94b0d885d21a13b41bf7510df78e
Patryk Stefański <119447102+S4ter@users.noreply.github.com>
2023-07-02 19:21:50 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import { App } from 'components/App';
-import { Profile } from './components/profile/Profile.jsx';
+import { Profile } from 'components/profile/Profile.jsx';
 import { Statistics } from 'components/statistics/Statistics.jsx';
 import { FriendList } from 'components/friendlist/FriendList.jsx';
-import { TransactionHistory } from './components/transactionhistory/TransactionHistory.jsx';
+import { TransactionHistory } from 'components/transactionhistory/TransactionHistory.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
